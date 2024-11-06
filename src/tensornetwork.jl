8959eb61a5432f58b34ec5af5902f91593391ccb
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-11-06 16:14:48 -0500

    WIP

--- src/tensornetwork.jl
+++ src/tensornetwork.jl
@@ -1,16 +1,30 @@
 using DataGraphs: DataGraphs, DataGraph
-using NamedGraphs: NamedGraphs, NamedEdge, NamedGraph, vertextype
+using NamedGraphs: NamedGraphs, NamedEdge, NamedGraph, vertextype, vertices
 
 """
 Generic tensor network data structure
 """
-mutable struct TensorNetwork
+mutable struct SimpleTensorNetwork
+    # data_graph: (undirected) graph of the tensor network
+    #   An integer is assigned to each vertex (starting from 1 and increasing one by one).
+    #   We can place an IndexedArray at each vertex of the graph, and an edge between two vertices.
+    #   But, the latter is not supported by the current implementation of SimpleTensorNetworks.jl.
+    #   This may be useful for supporting the Vidal notation.
     data_graph::DataGraph{Int,IndexedArray,IndexedArray,NamedGraph{Int},NamedEdge{Int}}
+end
+
+function SimpleTensorNetwork(ts::AbstractVector{<:AbstractIndexedArray})
+    g = NamedGraph(collect(eachindex(ts)))
+    tn = SimpleTensorNetwork(DataGraph{Int,IndexedArray,IndexedArray,NamedGraph{Int},NamedEdge{Int}}(g))
+    for v in vertices(g)
+      tn[v] = ts[v]
+    end
+    return tn
 end
 
 """
 Return if a tensor network `tn` is loop-free, i.e., `tn` is a tree tensor network
 """
-function isloopfree(tn::TensorNetwork)
-    return isloopfree(tn.data_graph)
+function isloopfree(tn::SimpleTensorNetwork)
+    # FIXME: Implement this function
 end
